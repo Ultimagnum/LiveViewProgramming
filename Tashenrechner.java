c841b4d4c805772fec159cd53e48079c58b3481a
Ultimagnum <oliverjkrueger@gmail.com>
2024-07-01 22:01:50 +0200

    Update Tashenrechner.java

---
+++ Tashenrechner.java
@@ -0,0 +1,23 @@
+import java.util.function.Function;
+
+Function<String,Numbers> toNumbers = (Function<String,Numbers>) n -> {if (n == "N") return new Zero();
+                                                    String m = n;
+                                                    m = m.replace("CM", "DCD");
+                                                    m = m.replace("M", "DD");
+                                                    m = m.replace("CD", "CCCC");
+                                                    m = m.replace("D", "CCCCC");
+                                                    m = m.replace("XC", "LXL");
+                                                    m = m.replace("C", "LL");
+                                                    m = m.replace("XL", "XXXX");
+                                                    m = m.replace("L", "XXXXX");
+                                                    m = m.replace("IX", "VIV");
+                                                    m = m.replace("X", "VV");
+                                                    m = m.replace("IV", "IIII");
+                                                    m = m.replace("V", "IIIII");
+
+                                                    Numbers myNumber = new Zero();
+                                                    for (int i = 0; i < m.length(); i++) {
+                                                        myNumber.addOne();
+                                                    }
+                                                    return myNumber;
+                                                    };
