dada666c4faf6277885b75642e5960d05c485217
Ultimagnum <oliverjkrueger@gmail.com>
2024-07-03 18:40:29 +0200

    Operator Rework

--- Operator.java
+++ Operator.java
@@ -3,43 +3,55 @@ import java.util.function.BiFunction;
 import javax.naming.OperationNotSupportedException;
 
 public enum Operator implements BiFunction<Numbers, Numbers, Numbers> {
-    ADD { public Numbers apply(Numbers a, Numbers b) { try {
-        return a.add(b);
-    } catch (OperationNotSupportedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+    ADD ((x,y) -> {
+        try {
+            return x.add(y);
+        } catch (OperationNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }),
+    SUB ((x,y) -> {
+        try {
+            return x.sub(y);
+        } catch (OperationNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }),
+    MUL ((x,y) -> {
+        try {
+            return x.mul(y);
+        } catch (OperationNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }),
+    DIV ((x,y) -> {
+        try {
+            return x.div(y);
+        } catch (OperationNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }),
+    MOD ((x,y) -> {
+        try {
+            return x.mod(y);
+        } catch (OperationNotSupportedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    });
+    final BiFunction<Numbers, Numbers, Numbers> op;
+    Operator(BiFunction<Numbers, Numbers, Numbers> op) {this.op = op;}
+    @Override
+    public Numbers apply(Numbers x, Numbers y) {
+        return op.apply(x, y);
     }
-    return new Zero(); }},
-
-    SUB { public Numbers apply(Numbers a, Numbers b) { try {
-        return a.sub(b);
-    } catch (OperationNotSupportedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-    return new Zero(); }},
-
-    MUL { public Numbers apply(Numbers a, Numbers b) { try {
-        return a.mul(b);
-    } catch (OperationNotSupportedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-    return new Zero(); }},
-
-    DIV { public Numbers apply(Numbers a, Numbers b) { try {
-        return a.div(b);
-    } catch (OperationNotSupportedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-    return new Zero(); }},
-
-    MOD { public Numbers apply(Numbers a, Numbers b) { try {
-        return a.mod(b);
-    } catch (OperationNotSupportedException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-    }
-    return new Zero(); }}
 }
